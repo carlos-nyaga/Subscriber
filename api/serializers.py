865500e02da1db4803d72f997ee83fad37286b50
4LG0R <crlsnyaga@gmail.com>
2019-01-10 21:48:46 +0300

    Found something new....ModelelSerializer & GenericViews.....just used them....code went to -50%....thank you Djangopython3.6 manage.py runserver!

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 
+from .models import Subscriber
 
-class SubscriberSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=50)
-    age = serializers.IntegerField()
-    email =  serializers.EmailField()
+
+class SubscriberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscriber
+        fields = "__all__"
--- api/views.py
+++ api/views.py
@@ -1,39 +1,11 @@
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST
-#from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView
+
+
 from .serializers import SubscriberSerializer
 from .models import Subscriber
 
 # Create your views here.
-class SubscriberView(APIView):
-    def get(self,request):
-        all_subscribers = Subscriber.objects.all()
-        serialized_subscribers = SubscriberSerializer(all_subscribers, many = True)
-        return Response(serialized_subscribers.data)
-
-    def post(self,request):
-        serializer = SubscriberSerializer(data=request.data)
-        if serializer.is_valid():
-            subscriber_instance = Subscriber.objects.create(**serializer.data)
-            return Response({"message": "Created subscriber {}" .format(subscriber_instance.id)})
-        else:
-            return Response({"errors": serializer.errors},status=HTTP_400_BAD_REQUEST)
-
-
-
-
-'''
-
-@api_view(["GET","POST"])
-def hello_world(request):
-    if request.method == "GET":
-        return Response({"message": "Hello World!"})
-    
-    else:
-        name = request.data.get("name")
-        if not name:
-            return Response({"error":"No name passed"},status=status.HTTP_400_BAD_REQUEST)
-        return Response({"message":"Hello {}!.format(name)"})
-'''
+class SubscriberView(ListCreateAPIView):
+    serializer_class = SubscriberSerializer
+    queryset = Subscriber.objects.all()
